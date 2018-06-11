857d89a07bec4ac4e502b27d87c36796d8625a81
Jackson Riso <risojackson@gmail.com>
2018-06-10 22:10:55 -0700

    Stock price scraper

--- app/workers/scrape_stock_price_worker.rb
+++ app/workers/scrape_stock_price_worker.rb
@@ -1,5 +1,5 @@
-# ScrapeStockPrice.perform_async("MSFT", "intraday")
-# ScrapeStockPrice.perform_async("MSFT", "daily")
+# ScrapeStockPriceWorker.perform_async("MSFT", "intraday")
+# ScrapeStockPriceWorker.perform_async("MSFT", "daily")
 
 require 'rubygems'
 require 'nokogiri'
@@ -9,17 +9,30 @@ class ScrapeStockPriceWorker
   include Sidekiq::Worker
 
   def perform(symbol, type)
-    api_key = '3TQBEBDIPSL35F38'
-    outputsize = 'full'
-    intraday_interval = '30min'
-    base_url = ''
+    # symbol = 'MSFT'
+    # type = 'daily'
+    apikey = '3TQBEBDIPSL35F38'
+    base_url = 'https://www.alphavantage.co/query'
 
     if type == 'intraday'
-      url = ''
+      url = base_url + '?symbol=' + symbol + '&function=TIME_SERIES_INTRADAY&interval=30min&outputsize=full&apikey=' + apikey
     elsif type == 'daily'
-      url = ''
+      url = base_url + '?symbol=' + symbol + '&function=TIME_SERIES_DAILY&outputsize=full&apikey=' + apikey
     end
-    puts symbol
-    puts type
+
+    HTTParty.get(url).values[1].each do |key, array|
+      # Get all the values
+      datetime = key
+      open = array['1. open']
+      close = array['4. close']
+      high = array['2. high']
+      low = array['3. low']
+      volume = array['5. volume']
+
+      # Check if the datetime and symbol already exists
+      unless Price.find_by(symbol: symbol, datetime: datetime)
+        Price.create(symbol: symbol, datetime: datetime, open: open, close: close, high: high, low: low, volume: volume)
+      end
+    end
   end
 end
