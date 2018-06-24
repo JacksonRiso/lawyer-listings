5c0b9cd87cf30039331e8626d7e22d08e3d90e91
Jackson Riso <risojackson@gmail.com>
2018-06-24 14:21:22 -0400

    Fix

--- lib/avvo.rb
+++ lib/avvo.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 module Avvo
-  def self.open_avvo_url
+  def self.open_avvo_url(url)
     Nokogiri::HTML(open(url, 'User-Agent' => 'firefox'))
   rescue OpenURI::HTTPError => error
     REDIS.set('avvo_scraper_last_error', Time.now)
