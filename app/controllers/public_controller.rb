124b688c2dfde3bf8d4901cc4c2739bbb2fab77a
Jackson Riso <risojackson@gmail.com>
2018-07-14 16:12:56 -0400

    Lawyers created by day

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,8 @@
 class PublicController < ApplicationController
   def index
+    # #Lawyers
     @number_of_lawyers = Lawyer.all.count
+    @lawyers_created_by_day = Lawyer.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('date', created_at)").count
     @number_of_urls = Url.all.count
     @number_of_stocks = Stock.all.count
     @number_of_prices = Price.all.count
