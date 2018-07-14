4f9a28ef551d3b7f177e83f6f2afe5014ad07815
Jackson Riso <risojackson@gmail.com>
2018-07-14 17:11:05 -0400

    Order by

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,18 +2,18 @@ class PublicController < ApplicationController
   def index
     # Lawyers
     @number_of_lawyers = Lawyer.all.count
-    @lawyers_created_by_day = Lawyer.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").count
+    @lawyers_created_by_day = Lawyer.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").order(:created_at).count
 
     # Urls
     @number_of_urls = Url.all.count
-    @urls_created_by_day = Url.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").count
+    @urls_created_by_day = Url.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").order(:created_at).count
 
     # Stocks
     @number_of_stocks = Stock.all.count
-    @stocks_created_by_day = Stock.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").count
+    @stocks_created_by_day = Stock.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").order(:created_at).count
 
     # Prices
     @number_of_prices = Price.all.count
-    @prices_created_by_day = Price.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").count
+    @prices_created_by_day = Price.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").order(:created_at).count
   end
 end
