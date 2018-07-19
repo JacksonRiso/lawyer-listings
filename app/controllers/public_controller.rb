c69669ec420a988a7fbe22c6ee752eef1e6ad020
Jackson Riso <risojackson@gmail.com>
2018-07-18 22:54:36 -0400

    First attempt

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,6 +4,10 @@ class PublicController < ApplicationController
     @number_of_lawyers = Lawyer.all.count
     @lawyers_created_by_day = Lawyer.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").order("DATE_TRUNC('day', created_at)").count
 
+    # Contact Methods
+    @number_of_phone_numbers = ContactMethod.where(contact_method_type: 'phone_number').count
+    @number_of_email_addresses = ContactMethod.where(contact_method_type: 'email').count
+
     # Urls
     @number_of_urls = Url.all.count
     @urls_created_by_day = Url.where('created_at >= ?', 1.week.ago).group("DATE_TRUNC('day', created_at)").order("DATE_TRUNC('day', created_at)").count
