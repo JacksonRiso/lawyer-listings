6cdccfcdcbe45731f69b681f8230aa1b9da90bc3
Jackson Riso <risojackson@gmail.com>
2018-07-23 17:20:41 -0400

    Background worker

--- app/workers/scrape_lawyer_website_worker.rb
+++ app/workers/scrape_lawyer_website_worker.rb
@@ -6,7 +6,7 @@ class ScrapeLawyerWebsiteWorker
   include Sidekiq::Worker
   sidekiq_options queue: :lawyer_website, retry: false, backtrace: false
 
-  def perform(url)
+  def perform(url, lawyer_id)
     url = 'https://plattner-verderame.com/attorneys/frank-verderame/'
     page = Nokogiri::HTML(open(url, 'User-Agent' => 'firefox'))
     unless page.nil?
@@ -14,6 +14,22 @@ class ScrapeLawyerWebsiteWorker
       facebook = page.css('a[href*="facebook.com"]')[0] ? page.css('a[href*="facebook.com"]')[0]['href'] : nil
       twitter = page.css('a[href*="twitter.com"]')[0] ? page.css('a[href*="twitter.com"]')[0]['href'] : nil
       linkedin = page.css('a[href*="linkedin.com"]')[0] ? page.css('a[href*="linkedin.com"]')[0]['href'] : nil
+
+      unless email && ContactMethod.find_by(info: email)
+        ContactMethod.create(lawyer_id: lawyer_id, contact_method_type: 'email', info: email)
+      end
+
+      unless facebook && ContactMethod.find_by(info: facebook)
+        ContactMethod.create(lawyer_id: lawyer_id, contact_method_type: 'facebook', info: facebook)
+      end
+
+      unless twitter_link && ContactMethod.find_by(info: twitter)
+        ContactMethod.create(lawyer_id: lawyer_id, contact_method_type: 'twitter', info: twitter)
+      end
+
+      unless linkedin_link && ContactMethod.find_by(info: linkedin)
+        ContactMethod.create(lawyer_id: lawyer_id, contact_method_type: 'linkedin', info: linkedin)
+      end
     end
   end
 end
