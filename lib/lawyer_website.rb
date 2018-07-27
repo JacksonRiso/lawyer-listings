6e5d06982e0148829e3c32c9fc8960183cb29a6c
Jackson Riso <risojackson@gmail.com>
2018-07-27 13:08:09 -0400

    Fix?

--- lib/lawyer_website.rb
+++ lib/lawyer_website.rb
@@ -4,6 +4,9 @@ require 'open-uri'
 
 module LawyerWebsite
   def self.open(url, lawyer_id)
+    puts 'URL GOES HERE'
+    puts url
+    puts 'URL GOES HERE END'
     Nokogiri::HTML(open(url, 'User-Agent' => 'firefox'))
   rescue OpenURI::HTTPError => error
     Lawyer.find_by(id: lawyer_id).update(website_status_code: error.status)
