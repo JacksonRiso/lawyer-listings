eeb2d0325f92cb8bf343459f3c1e3a2ff11b146a
Jackson Riso <risojackson@gmail.com>
2018-07-27 15:57:03 -0400

    Fix

--- app/models/contact_method.rb
+++ app/models/contact_method.rb
@@ -1,6 +1,6 @@
 class ContactMethod < ApplicationRecord
   def self.add_contact_method(contact_method_type, contact_method_info, lawyer_id)
-    if email && !ContactMethod.find_by(info: contact_method_info)
+    if contact_method_info && !ContactMethod.find_by(info: contact_method_info)
       ContactMethod.create(lawyer_id: lawyer_id, contact_method_type: contact_method_type, info: contact_method_info)
     end
   end
