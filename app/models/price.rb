dd374a328bf928e5190c4ce6b40cf520dbc0140d
Jackson Riso <risojackson@gmail.com>
2018-07-29 17:23:06 -0400

    Deploy?

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,5 +1,6 @@
 class Price < ActiveRecord::Base
   validates :unique_identifier, presence: true, uniqueness: true
+  validates :open, :close, :low, :high, numericality: { other_than: 0 }
   before_create :create_differences
   def create_differences
     self.percent_difference_between_open_and_close = ((open - close) / open) * 100
--- lib/tasks/add_differences_to_prices.rb
+++ lib/tasks/add_differences_to_prices.rb
@@ -1,5 +1,5 @@
 Price.where('percent_difference_between_open_and_close IS NULL').count
-Price.where('percent_difference_between_open_and_close IS NULL').limit(10_000).each do |price|
+Price.where('percent_difference_between_open_and_close IS NULL').limit(100_000).each do |price|
   percent_difference_between_open_and_close = ((price.open - price.close) / price.open) * 100
   percent_difference_between_low_and_high = ((price.low - price.high) / price.high) * 100
   Price.find_by(id: price.id).update(percent_difference_between_open_and_close: percent_difference_between_open_and_close.to_d.truncate(2),
