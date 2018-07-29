cbfa41245eef1a414687c4e981f71e40a452f56f
Jackson Riso <risojackson@gmail.com>
2018-07-29 18:10:32 -0400

    Fix?

--- lib/add_differences_to_prices.rb
+++ lib/add_differences_to_prices.rb
@@ -1,8 +1,2 @@
 while Price.where('percent_difference_between_open_and_close IS NULL').count > 0
-  Price.where('percent_difference_between_open_and_close IS NULL').limit(100_000).each do |price|
-    percent_difference_between_open_and_close = ((price.open - price.close) / price.open) * 100
-    percent_difference_between_low_and_high = ((price.low - price.high) / price.high) * 100
-    Price.find_by(id: price.id).update(percent_difference_between_open_and_close: percent_difference_between_open_and_close.to_d.truncate(2),
-                                       percent_difference_between_low_and_high: percent_difference_between_low_and_high.to_d.truncate(2))
-  end
 end
