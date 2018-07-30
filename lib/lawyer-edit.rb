0d439ffc5d7e4903aad0cbdd9a6ceccee4dc55ce
Jackson Riso <risojackson@gmail.com>
2018-07-29 23:02:08 -0400

    Edit

---
+++ lib/lawyer-edit.rb
@@ -0,0 +1,8 @@
+lawyers = []
+Lawyer.where(status: nil).order('RANDOM()').limit(110).each do |lawyer|
+  lawyers.push(lawyer.name + ',' + lawyer.avvo_url + ',' + lawyer.phone_number + ',' + lawyer.address)
+  lawyer.update(status: 'Assigned to royharber15 on 7/6/18')
+end
+lawyers.each do |lawyer|
+  puts lawyer
+end
