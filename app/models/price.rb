530df56e4796ac1f48bd5db133634f9f500f12e1
Jackson Riso <risojackson@gmail.com>
2018-07-29 23:25:51 -0400

    Push change to formula

--- app/models/price.rb
+++ app/models/price.rb
@@ -3,7 +3,7 @@ class Price < ActiveRecord::Base
   validates :open, :close, :low, :high, numericality: { other_than: 0 }
   before_create :create_differences
   def create_differences
-    self.percent_difference_between_open_and_close = ((open - close) / open) * 100
-    self.percent_difference_between_low_and_high = ((low - high) / high) * 100
+    self.percent_difference_between_open_and_close = ((close - open) / open) * 100
+    self.percent_difference_between_low_and_high = ((high - low) / low) * 100
   end
 end
