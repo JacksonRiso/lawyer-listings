183d49b908cfcff3997d9d05c284e656e48ab99d
Jackson Riso <risojackson@gmail.com>
2018-08-11 15:33:06 -0400

    Change

--- app/workers/scrape_stock_price_worker.rb
+++ app/workers/scrape_stock_price_worker.rb
@@ -17,30 +17,22 @@ class ScrapeStockPriceWorker
   def perform(symbol, stock_created_at)
     url = 'https://www.alphavantage.co/query?symbol=' + symbol + '&function=TIME_SERIES_DAILY&outputsize=full&apikey=3TQBEBDIPSL35F38'
 
-    begin
-      HTTParty.get(url).values[1].each do |key, array|
-        # Get all the values
-        datetime = key
-        open = array['1. open']
-        close = array['4. close']
-        high = array['2. high']
-        low = array['3. low']
-        volume = array['5. volume']
-        days_since_crawl = (stock_created_at.to_date - datetime.to_date).to_i
+    HTTParty.get(url).values[1].each do |key, array|
+      # Get all the values
+      datetime = key
+      open = array['1. open']
+      close = array['4. close']
+      high = array['2. high']
+      low = array['3. low']
+      volume = array['5. volume']
+      days_since_crawl = (stock_created_at.to_date - datetime.to_date).to_i
 
-        # Check if the datetime and symbol already exists
-        unless Price.find_by(symbol: symbol, datetime: datetime)
-          Price.create(symbol: symbol, datetime: datetime, open: open, close: close, high: high, low: low, volume: volume, days_since_crawl: days_since_crawl)
-        end
-        # Update the Stock
-        Stock.find_by(symbol: symbol).update(alpha_vantage: true, last_crawled: Time.now)
+      # Check if the datetime and symbol already exists
+      unless Price.find_by(symbol: symbol, datetime: datetime)
+        Price.create(symbol: symbol, datetime: datetime, open: open, close: close, high: high, low: low, volume: volume, days_since_crawl: days_since_crawl)
       end
-    rescue StandardError
-      puts 'There is no data for this stock in AlphaVantage'
-      puts symbol
-      puts url
-      Stock.find_by(symbol: symbol).update(alpha_vantage: false)
-      puts "Updated stock so it won't be crawled again"
+      # Update the Stock
+      Stock.find_by(symbol: symbol).update(alpha_vantage: true, last_crawled: Time.now)
     end
   end
 end
